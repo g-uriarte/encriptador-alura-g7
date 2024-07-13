6c1da4bf07fa81c4be2fed9b3df43e9992de7883
g-uriarte <gabriel.agustin.uriarte@gmail.com>
2024-07-12 21:07:46 -0300

    changed text validation, now just check if the text has only lowercase characters

--- src/js/app.js
+++ src/js/app.js
@@ -14,30 +14,23 @@ const app = (ui, encryptionMode) => {
 	 * @param {string} text
 	 */
 	const validateText = (text) => {
-		const messages = [];
-		if (text === "") {
-			messages.push("Introduzca un texto a codificar.");
-		}
-		const hasUppercaseLetter = /[A-Z]/.test(text);
-		if (hasUppercaseLetter) {
-			messages.push("No se permiten letras mayusculas.");
-		}
-		const hasAccentLetters = /[áéíóúÁÉÍÓÚ]/.test(text);
-		if (hasAccentLetters) {
-			messages.push("No se permiten letras con tilde.");
-		}
-
-		if (messages.length > 0) {
+		if (text === '') {
 			return {
 				error: true,
-				messages: messages,
-			};
-		} else {
+				message: 'Por favor introduzca un texto.'
+			}
+		}
+
+		const hasValidText = /^[a-z]+$/g.test(text);
+		if (!hasValidText) {
 			return {
-				error: false,
-				messages: messages,
+				error: true,
+				message: 'Por favor solo introduzca letras minúsculas, sin acentos y sin caracteres especiales.',
 			};
 		}
+		return {
+			error: false,
+		};
 	};
 
 	/**
@@ -46,10 +39,10 @@ const app = (ui, encryptionMode) => {
 	 */
 	const handleAction = (type) => {
 		const text = ui.getTextAreaValue().trim();
-		const { error, messages } = validateText(text);
+		const { error, message } = validateText(text);
 		if (error) {
 			ui.markTextAreaError();
-			Toast.create(messages.map(message => `<p style='font-size: 15px'>${message}</p>`).join(''), 'error')
+			Toast.create(message, 'error')
 		} else {
 			const resultText = type === 'encript' ? encryptionMode.encript(text) : encryptionMode.decript(text);
 			if (!ui.hasInteraction()) {
